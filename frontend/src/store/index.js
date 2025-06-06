c16a4de8fe8dbbc14ec5085f8b83c118291eb5a7
trianandafajar <mail.trianandafajar@gmail.com>
2025-06-06 20:08:59 +0700

    Update store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,17 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
 import categoryReducer from "../features/CategoriSlice";
 import productReducer from "../features/ProductSlice";
 import cartReducer from "../features/CartSlice";
 
+// Konfigurasi persist
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'], // Hanya cart yang perlu di-persist
+};
+
+const rootReducer = combineReducers({
+  category: categoryReducer,
+  product: productReducer,
+  cart: cartReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    category: categoryReducer,
-    product: productReducer,
-    cart: cartReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false, // Disable warning kalau pakai localStorage, FormData, dll
+      serializableCheck: {
+        // Mengabaikan warning untuk action non-serializable
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+      immutableCheck: false, // Menonaktifkan immutable check untuk performa
     }),
-  devTools: process.env.NODE_ENV !== "production", // Enable Redux DevTools hanya di dev
+  devTools: process.env.NODE_ENV !== "production",
 });
+
+export const persistor = persistStore(store);
